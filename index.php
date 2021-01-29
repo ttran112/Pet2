af4f320b4289a3f1bbc7cd3dc92bacab63cfcf03
ttran112 <ttran112@mail.greenriver.edu>
2021-01-29 15:51:43 -0500

    new update for 1st order and image and header helper

--- index.php
+++ index.php
@@ -15,9 +15,15 @@ $f3 -> set('DEBUG',3);
 
 $f3 -> route('GET /', function ()
 {
-   // echo '<h1>Pet 2</h1>';
     $view = new Template();
     echo $view -> render('views/pet-home.html');
+}
+);
+
+$f3 -> route('GET /order', function ()
+{
+    $view = new Template();
+    echo $view -> render('views/pet-order.html');
 }
 );
 $f3 -> run();
